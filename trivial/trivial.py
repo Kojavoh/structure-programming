e61fa63d759ea6403f3c86160f3847979593f082
Kojavoh <chartm17@kent.edu>
2023-09-11 16:46:55 -0400

    finishing classwork

--- trivial/trivial.py
+++ trivial/trivial.py
@@ -14,19 +14,30 @@ def resolve(s, e):
     else:
         return None
 
+def set_statement(v, e):
+    assert len(v) == 3
+    assert v[0] == "set"
+    assert type(v[1]) is str
+    e[v[1]] = evaluate(v[2], e)
+    return None
 
-def evaluate(v, env):
+def evaluate(v, e):
     if type(v) is list:
         if len(v) == 0:
             return None
         assert type(v[0]) is str
-        f = resolve(v[0], env)
+        if v[0] == "set":
+            return set_statement(v, e)
+        # if v[0] == "set":
+        #     return set_statement(v, e)
+        
+        f = resolve(v[0], e)
         assert callable(f)
-        values = [evaluate(value, env) for value in v[1:]]
+        values = [evaluate(value, e) for value in v[1:]]
         # Expression called a "Comprehension" ^^^
-        return f(values, env)
+        return f(values, e)
     if type(v) is str:
-        return resolve(v, env)
+        return resolve(v, e)
     return v
 
 def test_evaluate():
@@ -39,6 +50,13 @@ def test_evaluate():
     assert evaluate(['-', 33, 22], env) == 11
     assert evaluate(['*', 10, 22], env) == 220
     assert evaluate(['/', 220, 10], env) == 22
+    assert evaluate(['set', 'q', 7], env) == None
+    assert env['q'] == 7
+    assert evaluate(['set', 'q', 77], env) == None
+    assert env['q'] == 77
+    assert evaluate(['set', 'q', ['+', 5, 6]], env) == None
+    assert env['q'] == 11
+    evaluate(['print', 2, 3, 4], env)
 
 def test_resolve():
     print("test_resolve")
